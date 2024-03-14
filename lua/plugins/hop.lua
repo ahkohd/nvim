47adcdb5879d64c549e26c2dd9712c7070f5b7de
Victor Aremu <me@victorare.mu>
2024-03-14 21:39:27 +0100

    feat: update hop.nvim

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,32 +1,37 @@
 return {
-  "phaazon/hop.nvim",
-  branch = "v2",
-  keys = { "f" },
-  config = function()
-    require("hop").setup()
-    vim.api.nvim_set_keymap(
-      "",
-      "f",
-      "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>",
-      {}
-    )
-    vim.api.nvim_set_keymap(
-      "",
-      "F",
-      "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>",
-      {}
-    )
-    vim.api.nvim_set_keymap(
-      "",
-      "t",
-      "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>",
-      {}
-    )
-    vim.api.nvim_set_keymap(
-      "",
-      "T",
-      "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>",
-      {}
-    )
-  end,
+	"smoka7/hop.nvim",
+	version = "*",
+	event = "BufRead",
+	keys = {
+		{
+			"f",
+			"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>",
+			desc = "Jump to character after cursor",
+		},
+		{
+			"F",
+			"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>",
+			desc = "Jump to character before cursor",
+		},
+		{
+			"t",
+			"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>",
+			desc = "Jump to the character before the target character after cursor",
+		},
+		{
+			"T",
+			"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>",
+			desc = "Jump to the character after the target character before cursor",
+		},
+		{
+			"<space>fj",
+			"<cmd>lua require'hop'.hint_words()<cr>",
+			desc = "Jump to word",
+			mode = { "n", "x", "o" },
+		},
+	},
+	opts = {
+		multi_windows = true,
+		keys = "etovxqpdygfblzhckisuran",
+	},
 }
