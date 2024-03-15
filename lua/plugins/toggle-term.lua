c673056875a3ed7030a40dea5f1755beb8c9de62
Victor Aremu <me@victorare.mu>
2024-03-15 09:36:01 +0100

    feat: add mprocs

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -6,6 +6,13 @@ return {
 	version = "*",
 	keys = {
 		{ "<space><tab>", desc = "Open Terminal" },
+		{
+			"<space>q",
+			function()
+				_mprocs_toggle()
+			end,
+			desc = "Toggle mprocs",
+		},
 	},
 	config = function()
 		local toggleterm = require("toggleterm")
@@ -20,5 +27,30 @@ return {
 				border = "curved",
 			},
 		})
+
+		local Terminal = require("toggleterm.terminal").Terminal
+
+		local mprocs = Terminal:new({
+			cmd = "mprocs",
+			direction = "float",
+			on_open = function(term)
+				vim.cmd("startinsert!")
+
+				vim.api.nvim_buf_set_keymap(
+					term.bufnr,
+					"t",
+					"<space>q",
+					"<cmd>close<CR>",
+					{ noremap = true, silent = true }
+				)
+			end,
+			on_close = function(term)
+				vim.cmd("startinsert!")
+			end,
+		})
+
+		function _mprocs_toggle()
+			mprocs:toggle()
+		end
 	end,
 }
