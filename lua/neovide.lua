e3e34f6eef67a24da3b97d402591cc9246496a89
Victor Aremu <me@victorare.mu>
2024-05-12 22:24:13 +0100

    feat: update neovide configuration

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,4 +1,5 @@
-vim.o.guifont = "BitstromWera Nerd Font:h15:#e-subpixelantialias"
+vim.o.guifont =
+	"BerkeleyMono Nerd Font Plus Font Awesome Plus Octicons Plus Codicons Plus Material Design Icons:h15:#e-subpixelantialias"
 
 vim.g.neovide_refresh_rate = 120
 
@@ -6,8 +7,41 @@ vim.g.neovide_confirm_quit = false
 
 vim.g.neovide_cursor_vfx_mode = "sonicboom"
 
-vim.g.neovide_floating_shadow = false
-
 vim.g.neovide_cursor_trail_size = 0.0
 
 vim.g.neovide_cursor_animation_length = 0.15
+
+vim.g.neovide_remember_window_size = true
+
+-- spacing
+vim.g.neovide_padding_top = 5
+
+vim.g.neovide_padding_bottom = 0
+
+vim.g.neovide_padding_right = 10
+
+vim.g.neovide_padding_left = 10
+
+-- window appearance (macOS)
+
+vim.g.neovide_window_blurred = true
+
+vim.g.neovide_transparency = 0.9
+
+vim.g.transparency = 0.8
+
+vim.g.neovide_show_border = false
+
+-- text rendering
+
+vim.g.neovide_text_gamma = 0.8
+
+vim.g.neovide_text_contrast = 0.1
+
+-- float
+
+vim.g.neovide_floating_blur_amount_x = 2.0
+
+vim.g.neovide_floating_blur_amount_y = 2.0
+
+vim.g.neovide_floating_shadow = false
