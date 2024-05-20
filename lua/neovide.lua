ed63c9f95821077bcd98cb79516ac65d80c22ddd
Victor Aremu <me@victorare.mu>
2024-05-20 03:19:14 +0100

    feat: increase cursor animation duration

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -7,7 +7,7 @@ vim.g.neovide_confirm_quit = false
 
 vim.g.neovide_cursor_vfx_mode = "sonicboom"
 
-vim.g.neovide_cursor_trail_size = 0.0
+vim.g.neovide_cursor_trail_size = 0.05
 
 vim.g.neovide_cursor_animation_length = 0.0
 
