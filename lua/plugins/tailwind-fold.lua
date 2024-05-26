a554e0491616610e92e7f4fe01989113b514274a
Victor Aremu <me@victorare.mu>
2024-05-26 22:56:47 +0100

    feat: add tailwind fold

---
+++ lua/plugins/tailwind-fold.lua
@@ -0,0 +1,7 @@
+return {
+	"razak17/tailwind-fold.nvim",
+	opts = {},
+	event = "VeryLazy",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	ft = { "html", "svelte", "astro", "vue", "typescriptreact", "php", "blade" },
+}
