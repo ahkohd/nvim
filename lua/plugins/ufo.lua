27aca49e08a2c99ed9dc8c0235aefdfef44d0bb5
Victor Aremu <me@victorare.mu>
2024-06-03 00:03:48 +0100

    refactor: ufo

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -5,11 +5,31 @@ return {
 	dependencies = { "kevinhwang91/promise-async" },
 	event = { "BufReadPre", "BufNewFile" },
 	keys = {
-		{ "zR", "<cmd>:lua require('ufo').openAllFolds()<CR>", desc = "Open all folds" },
-		{ "zM", "<cmd>:lua require('ufo').closeAllFolds()<CR>", desc = "Close all folds" },
-		{ "zr", "<cmd>:lua require('ufo').openFoldsExceptKinds()<CR>", desc = "Open all folds except kinds" },
-		{ "zm", "<cmd>:lua require('ufo').closeFoldsWith()<CR>", desc = "Close all folds with kinds" },
-		{ "K", desc = "Peek folded lines under cursor" },
+		{
+			"zR",
+			function()
+				require("ufo").openAllFolds()
+			end,
+			desc = "Open all folds",
+		},
+		{
+			"zM",
+			function()
+				require("ufo").closeAllFolds()
+			end,
+			desc = "Close all folds",
+		},
+		{
+			"K",
+			function()
+				local winid = require("ufo").peekFoldedLinesUnderCursor()
+
+				if not winid then
+					vim.lsp.buf.hover()
+				end
+			end,
+			desc = "Peek folded line under cursor",
+		},
 	},
 	config = function()
 		local ftMap = {
@@ -22,23 +42,33 @@ return {
 		vim.o.foldlevel = 99
 		vim.o.foldlevelstart = 99
 		vim.o.foldenable = true
-		vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+		vim.o.fillchars = [[eob: ,fold: ,foldopen: ,foldsep: ,foldclose: ]]
 
 		local handler = function(virtText, lnum, endLnum, width, truncate)
 			local newVirtText = {}
+
 			local suffix = (" ↙%d "):format(endLnum - lnum)
+
 			local sufWidth = vim.fn.strdisplaywidth(suffix)
+
 			local targetWidth = width - sufWidth
+
 			local curWidth = 0
+
 			for _, chunk in ipairs(virtText) do
 				local chunkText = chunk[1]
+
 				local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+
 				if targetWidth > curWidth + chunkWidth then
 					table.insert(newVirtText, chunk)
 				else
 					chunkText = truncate(chunkText, targetWidth - curWidth)
+
 					local hlGroup = chunk[2]
+
 					table.insert(newVirtText, { chunkText, hlGroup })
+
 					chunkWidth = vim.fn.strdisplaywidth(chunkText)
 					-- str width returned from truncate() may less than 2nd argument, need padding
 					if curWidth + chunkWidth < targetWidth then
@@ -48,7 +78,9 @@ return {
 				end
 				curWidth = curWidth + chunkWidth
 			end
+
 			table.insert(newVirtText, { suffix, "MoreMsg" })
+
 			return newVirtText
 		end
 
@@ -73,13 +105,6 @@ return {
 				return ftMap[filetype] or { "lsp", "indent" }
 			end,
 		})
-
-		vim.keymap.set("n", "K", function()
-			local winid = require("ufo").peekFoldedLinesUnderCursor()
-			if not winid then
-				vim.lsp.buf.hover()
-			end
-		end)
 
 		vim.cmd([[highlight Folded guibg=NONE ctermbg=NONE]])
 	end,
