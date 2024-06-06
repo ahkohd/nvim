ab95e793bc7b66d5599b7d80fbfae835883c13f1
Victor Aremu <me@victorare.mu>
2024-06-06 21:47:08 +0100

    feat: add gruvbox light theme

--- lua/plugins/themery.lua
+++ lua/plugins/themery.lua
@@ -23,9 +23,43 @@ return {
           ]],
 				},
 				{
-					name = "Gruvbox",
+					name = "Gruvbox (dark)",
 					colorscheme = "gruvbox",
 					before = [[
+            vim.opt.background = "dark"
+
+            require("gruvbox").setup({
+                terminal_colors = true, -- add neovim terminal colors
+                undercurl = true,
+                underline = true,
+                bold = true,
+                italic = {
+                  strings = true,
+                  emphasis = true,
+                  comments = true,
+                  operators = false,
+                  folds = true,
+                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                invert_intend_guides = false,
+                inverse = true, -- invert background for search, diffs, statuslines and errors
+                contrast = "", -- can be "hard", "soft" or empty string
+                palette_overrides = {},
+                overrides = {},
+                dim_inactive = false,
+                transparent_mode = false,
+            })
+          ]],
+				},
+				{
+					name = "Gruvbox (light)",
+					colorscheme = "gruvbox",
+					before = [[
+            vim.opt.background = "light"
+
             require("gruvbox").setup({
                 terminal_colors = true, -- add neovim terminal colors
                 undercurl = true,
