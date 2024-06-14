cc22e15220dfe20d13c0f13db471ebe573abb694
Victor Aremu <me@victorare.mu>
2024-06-14 11:01:45 +0100

    feat: add gh keybindings

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -3,15 +3,6 @@
 return {
 	"kdheepak/lazygit.nvim",
 	event = "VeryLazy",
-	keys = {
-		{
-			"<space>g",
-			function()
-				vim.cmd("LazyGit")
-			end,
-			desc = "Toggle LazyGit",
-		},
-	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
