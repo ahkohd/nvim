91ea8d51c33737b5a64d8d5a3a74a140b4e073e6
Victor Aremu <me@victorare.mu>
2024-06-15 01:04:38 +0100

    refactor: remove deprecated option

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -87,7 +87,11 @@ return {
 		require("ufo").setup({
 			fold_virt_text_handler = handler,
 			open_fold_hl_timeout = 150,
-			close_fold_kinds = { "imports", "comment" },
+			close_fold_kinds_for_ft = {
+				default = { "imports", "comment" },
+				json = { "array" },
+				c = { "comment", "region" },
+			},
 			preview = {
 				win_config = {
 					border = { "", "─", "", "", "", "─", "", "" },
