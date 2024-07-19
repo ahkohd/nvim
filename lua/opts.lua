76612a35fe622ff4bcdd18b953e7e3080e499b96
Victor Aremu <me@victorare.mu>
2024-07-19 02:37:52 +0100

    refactor: remove neovide

--- lua/opts.lua
+++ lua/opts.lua
@@ -70,7 +70,3 @@ vim.g["pencil#conceallevel"] = 2
 
 -- register command that calls a lua function
 vim.cmd("command! -nargs=0 ProjectRoot lua require('core.utils.project').cwd_to_root_dir_of_current_buffer()")
-
-if vim.g.neovide then
-	require("neovide")
-end
