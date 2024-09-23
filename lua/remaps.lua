0ff101cad1dd7e2138884190ebde64d73961b8ab
Victor Aremu <me@victorare.mu>
2024-09-23 02:03:40 +0100

    feat: auto center jumps to screen center

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -3,5 +3,8 @@ local map = vim.api.nvim_set_keymap
 map("n", "<C-d>", "<C-d>zz", { noremap = true })
 map("n", "<C-u>", "<C-u>zz", { noremap = true })
 
+map("n", "<C-i>", "<C-i>zz", { noremap = true })
+map("n", "<C-o>", "<C-o>zz", { noremap = true })
+
 map("n", "n", "nzzzv", { noremap = true })
 map("n", "N", "Nzzzv", { noremap = true })
