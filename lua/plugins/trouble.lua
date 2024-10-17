455a8614d3cb24054c3461fdc2f4dc0360d22b54
Victor Aremu <me@victorare.mu>
2024-10-17 20:35:29 +0100

    refactor: document symbols

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,3 +1,5 @@
+-- luacheck: globals vim
+
 return {
 	"folke/trouble.nvim",
 	opts = {},
@@ -12,6 +14,16 @@ return {
 			"<space>cX",
 			"<cmd>Trouble diagnostics toggle<cr>",
 			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<space>cl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<space>cs",
+			"<cmd>Trouble lsp_document_symbols focus=false win.position=right<cr>",
+			desc = "Toggle Buffer Symbols",
 		},
 	},
 }
