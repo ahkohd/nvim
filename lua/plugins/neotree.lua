1335895c703751f7898474f90dea3a04564d8315
Victor Aremu <me@victorare.mu>
2024-11-08 22:05:49 +0100

    feat: install neotree

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,16 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	keys = {
+		{
+			"<space>_",
+			"<cmd>Neotree filesystem reveal current<cr>",
+			desc = "Toggle Neotree",
+		},
+	},
+}
