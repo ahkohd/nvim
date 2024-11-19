92fa17bf3666e9e9e1f710c54ceac62e2ddabe9e
Victor Aremu <me@victorare.mu>
2024-11-19 22:26:40 +0100

    fix: leader for neotree keybind

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,8 +9,8 @@ return {
 	},
 	event = "VeryLazy",
 	keys = {
-		{ "<leader>e", ":Neotree toggle float<CR>", silent = true, desc = "Float File Explorer" },
-		{ "<leader>E", ":Neotree toggle left<CR>", silent = true, desc = "Left File Explorer" },
+		{ "<space>e", ":Neotree toggle float<CR>", silent = true, desc = "Float File Explorer" },
+		{ "<space>E", ":Neotree toggle left<CR>", silent = true, desc = "Left File Explorer" },
 	},
 	config = function()
 		require("neo-tree").setup({
