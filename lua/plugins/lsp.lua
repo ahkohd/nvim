4aa38c4b835859683fa58695aa492e04e3f9823c
Victor Aremu <me@victorare.mu>
2024-11-20 13:39:40 +0100

    development
    refactor: remove debugger

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,11 +7,6 @@ return {
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
 		"williamboman/mason.nvim",
-
-		"mfussenegger/nvim-dap",
-		"nvim-neotest/nvim-nio",
-
-		"jay-babu/mason-nvim-dap.nvim",
 		{
 			"mrcjkb/rustaceanvim",
 			version = "^5",
@@ -129,27 +124,9 @@ return {
 
 		-- configure rustaceanvim
 		local rust_config = function()
-			local extension_path = vim.env.HOME .. "/.local/share/nvim/mason/packages/codelldb/extension/"
-			local codelldb_path = extension_path .. "adapter/codelldb"
-			local liblldb_path = extension_path .. "lldb/lib/liblldb"
-			local this_os = vim.loop.os_uname().sysname
-
-			-- The path is different on Windows
-			if this_os:find("Windows") then
-				codelldb_path = extension_path .. "adapter\\codelldb.exe"
-				liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
-			else
-				-- The liblldb extension is .so for Linux and .dylib for MacOS
-				liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
-			end
-
-			local cfg = require("rustaceanvim.config")
 			local lsp_utils = require("core.utils.lsp")
 
 			return {
-				dap = {
-					adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-				},
 				server = {
 					on_attach = lsp_utils.on_attach,
 					default_settings = {
@@ -162,121 +139,5 @@ return {
 		end
 
 		vim.g.rustaceanvim = rust_config()
-
-		-- configure DAP
-		require("mason-nvim-dap").setup({
-			ensure_installed = { "js", "codelldb" },
-		})
-
-		local js_dap_config = function()
-			local dap = require("dap")
-
-			local js_based_languages = { "typescript", "javascript", "typescriptreact" }
-
-			local js_debug_adapter_path = require("mason-registry").get_package("js-debug-adapter"):get_install_path()
-
-			dap.set_log_level("DEBUG")
-
-			for _, adapter in ipairs({ "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }) do
-				dap.adapters[adapter] = {
-					type = "server",
-					host = "localhost",
-					port = "${port}",
-					executable = {
-						command = "node",
-						args = {
-							js_debug_adapter_path .. "/js-debug/src/dapDebugServer.js",
-							"${port}",
-						},
-					},
-				}
-			end
-
-			dap.adapters["pwa-chrome"] = {
-				type = "server",
-				host = "localhost",
-				port = "${port}",
-				executable = {
-					command = "node",
-					args = {
-						js_debug_adapter_path .. "/js-debug/src/dapDebugServer.js",
-						"${port}",
-					},
-				},
-			}
-
-			for _, language in ipairs(js_based_languages) do
-				require("dap").configurations[language] = {
-					{
-						type = "pwa-node",
-						request = "launch",
-						name = "Launch Current File (pwa-node)",
-						program = "${file}",
-						cwd = "${workspaceFolder}",
-						skipFiles = { "<node_internals>/**" },
-						resolveSourceMapLocations = {
-							"${workspaceFolder}/**",
-							"!**/node_modules/**",
-						},
-					},
-					{
-						type = "pwa-node",
-						request = "attach",
-						name = "Attach Program (pwa-node, select pid)",
-						program = "${file}",
-						cwd = "${workspaceFolder}",
-						processId = require("dap.utils").pick_process,
-						skipFiles = { "<node_internals>/**" },
-						resolveSourceMapLocations = {
-							"${workspaceFolder}/**",
-							"!**/node_modules/**",
-						},
-					},
-					{
-						type = "pwa-chrome",
-						request = "attach",
-						name = "Attach Program (pwa-chrome, select port)",
-						program = "${file}",
-						cwd = "${workspaceFolder}",
-						sourceMaps = true,
-						protocol = "inspector",
-						port = function()
-							return vim.fn.input("Select port: ", 9222)
-						end,
-						webRoot = "${workspaceFolder}",
-					},
-					{
-						type = "pwa-chrome",
-						request = "launch",
-						name = 'Launch Chrome with "localhost"',
-						url = function()
-							local co = coroutine.running()
-							return coroutine.create(function()
-								vim.ui.input(
-									{ prompt = "Enter URL: ", default = "http://localhost:5173" },
-									function(url)
-										if url == nil or url == "" then
-											return
-										else
-											coroutine.resume(co, url)
-										end
-									end
-								)
-							end)
-						end,
-						webRoot = "${workspaceFolder}",
-						protocol = "inspector",
-						sourceMaps = true,
-						userDataDir = false,
-						resolveSourceMapLocations = {
-							"${workspaceFolder}/**",
-							"!**/node_modules/**",
-						},
-					},
-				}
-			end
-		end
-
-		js_dap_config()
 	end,
 }
