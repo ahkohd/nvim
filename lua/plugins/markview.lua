5eac42e385a5f872bb1cfda2d7e0bb8b705fabee
Victor Aremu <me@victorare.mu>
2024-11-24 03:53:42 +0100

    pref: enable markdown renderer only when
    the buffer file type is markdown

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,6 +1,6 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
-	event = "VeryLazy",
+	ft = "markdown",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"nvim-tree/nvim-web-devicons",
