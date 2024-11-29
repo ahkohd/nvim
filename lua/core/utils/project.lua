8f278df6824710da6f45e95097c6d0413260e49d
Victor Aremu <me@victorare.mu>
2024-11-29 20:40:38 +0100

    feat: replace neoclip with fzf-lua

--- lua/core/utils/project.lua
+++ lua/core/utils/project.lua
@@ -24,20 +24,6 @@ M.cwd_to_root_dir_of_current_buffer = function()
 	M.cwd_to_root_dir()
 end
 
-M.open_project = function(project_path)
-	local actions = require("telescope.actions")
-	require("telescope.builtin").find_files({
-		cwd = project_path,
-		attach_mappings = function(prompt_bufnr, _)
-			actions.select_default:replace(function()
-				actions.file_edit(prompt_bufnr)
-				M.cwd_to_root_dir_of_current_buffer()
-			end)
-			return true
-		end,
-	})
-end
-
 M.open_file = function(file_path)
 	vim.cmd("edit " .. file_path)
 	M.cwd_to_root_dir()
---
+++ lua/plugins/fzf.lua
@@ -0,0 +1,52 @@
+return {
+	"ibhagwan/fzf-lua",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		{
+			"AckslD/nvim-neoclip.lua",
+			name = "neoclip",
+			opts = {},
+		},
+	},
+	cmd = "FzfLua",
+	keys = {
+		{
+			"<space>f",
+			"<cmd>FzfLua files<cr>",
+			desc = "Find files",
+		},
+		{
+			"<space>r",
+			"<cmd>FzfLua live_grep_native<cr>",
+			desc = "Live grep",
+		},
+		{
+			"<space>j",
+			"<cmd>FzfLua jumps<cr>",
+			desc = "Search jumplist",
+		},
+		{
+			"<space><space>",
+			"<cmd>FzfLua buffers<cr>",
+			desc = "Search buffers",
+		},
+		{
+			"<space>\\",
+			"<cmd>FzfLua helptags<cr>",
+			desc = "Search helptags",
+		},
+		{
+			"<space>-",
+			"<cmd>NoiceFzf<cr>",
+			desc = "Search messages",
+		},
+		{
+			"<space>p",
+			function()
+				require("neoclip.fzf")()
+			end,
+			desc = "Search yank list",
+		},
+	},
+	opts = {},
+}
