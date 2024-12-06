07e1d9e47fb84bba86ce6b2d3cb8652b82616737
Victor Aremu <me@victorare.mu>
2024-12-06 21:55:27 +0100

    feat: add trouble

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,34 @@
+-- luacheck: globals vim
+return {
+	"folke/trouble.nvim",
+	cmd = "Trouble",
+	opts = {
+		modes = {
+			preview_float = {
+				mode = "diagnostics",
+				preview = {
+					type = "float",
+					relative = "editor",
+					border = "rounded",
+					title = "Preview",
+					title_pos = "center",
+					position = { 0, -2 },
+					size = { width = 0.5, height = 0.5 },
+					zindex = 200,
+				},
+			},
+		},
+	},
+	keys = {
+		{
+			"<space>t",
+			"<cmd>Trouble preview_float toggle focus=true filter.buf=0<cr>",
+			desc = "Toggle Trouble (Buffer)",
+		},
+		{
+			"<space>T",
+			"<cmd>Trouble preview_float toggle focus=true<cr>",
+			desc = "Toggle Trouble (Workspace)",
+		},
+	},
+}
