c7223a8a63caa3986453b532ec7fbc88edd54c46
Victor Aremu <me@victorare.mu>
2024-12-21 08:33:36 +0100

    refactor: blink.cmp config

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -3,21 +3,60 @@
 return {
 	"saghen/blink.cmp",
 	event = { "LspAttach", "InsertCharPre" },
-	dependencies = "rafamadriz/friendly-snippets",
 	version = "v0.*",
 	opts = {
-		nerd_font_variant = "mono",
-
-		-- experimental auto-brackets support
-		-- accept = { auto_brackets = { enabled = true } },
-
-		-- experimental signature help support
-		trigger = {
-			-- completion = {
-			-- 	keyword_range = "prefix",
-			-- },
-			signature_help = { enabled = true },
+		appearance = {
+			nerd_font_variant = "mono",
+			kind_icons = {
+				Text = "Text",
+				Method = "Method",
+				Function = "Function",
+				Constructor = "Constructor",
+				Field = "Field",
+				Variable = "Variable",
+				Class = "Class",
+				Interface = "Interface",
+				Module = "Module",
+				Property = "Property",
+				Unit = "Unit",
+				Value = "Value",
+				Enum = "Enum",
+				Keyword = "Keyword",
+				Snippet = "Snippet",
+				Color = "Color",
+				Reference = "Reference",
+				File = "File",
+				Folder = "Folder",
+				EnumMember = "EnumMember",
+				Constant = "Constant",
+				Struct = "Struct",
+				Event = "Event",
+				Operator = "Operator",
+				TypeParameter = "TypeParameter",
+			},
 		},
+
+		completion = {
+			keyword = { range = "full" },
+			accept = { auto_brackets = { enabled = false } },
+			list = { selection = "auto_insert", cycle = { from_bottom = true, from_top = false } },
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 0,
+				window = {
+					min_width = 15,
+					max_width = 50,
+					max_height = 15,
+					direction_priority = {
+						menu_north = { "e", "w" },
+						menu_south = { "e", "w", "s" },
+					},
+				},
+			},
+			ghost_text = { enabled = true },
+		},
+
+		signature = { enabled = true },
 
 		keymap = {
 			preset = "default",
@@ -26,51 +65,6 @@ return {
 			["<c-k>"] = { "select_prev" },
 			["<c-d>"] = { "scroll_documentation_down" },
 			["<c-u>"] = { "scroll_documentation_up" },
-		},
-
-		windows = {
-			documentation = {
-				min_width = 15,
-				max_width = 50,
-				max_height = 15,
-				auto_show = true,
-				auto_show_delay_ms = 0,
-				direction_priority = {
-					autocomplete_north = { "e", "w" },
-					autocomplete_south = { "e", "w", "s" },
-				},
-			},
-			autocomplete = {
-				cycle = { from_top = false },
-			},
-		},
-
-		kind_icons = {
-			Text = "Text",
-			Method = "Method",
-			Function = "Function",
-			Constructor = "Constructor",
-			Field = "Field",
-			Variable = "Variable",
-			Class = "Class",
-			Interface = "Interface",
-			Module = "Module",
-			Property = "Property",
-			Unit = "Unit",
-			Value = "Value",
-			Enum = "Enum",
-			Keyword = "Keyword",
-			Snippet = "Snippet",
-			Color = "Color",
-			Reference = "Reference",
-			File = "File",
-			Folder = "Folder",
-			EnumMember = "EnumMember",
-			Constant = "Constant",
-			Struct = "Struct",
-			Event = "Event",
-			Operator = "Operator",
-			TypeParameter = "TypeParameter",
 		},
 	},
 }
