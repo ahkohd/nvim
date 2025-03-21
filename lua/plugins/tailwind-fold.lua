6bd0625ff27a5da5c39324771ddcc01d6908e451
Victor Aremu <me@victorare.mu>
2025-03-21 23:56:25 +0000

    remove tailwind-fold

--- lua/plugins/tailwind-fold.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"razak17/tailwind-fold.nvim",
-	ft = { "html", "astro", "javascriptreact", "typescriptreact" },
-	dependencies = { "nvim-treesitter/nvim-treesitter" },
-	opts = {
-		symbol = "󱏿",
-	},
-}
