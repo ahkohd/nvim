8c150be3956314ca6fc5726b16697fae30ffc63c
Victor Aremu <me@victorare.mu>
2025-06-23 20:12:17 +0100

    wip

---
+++ lua/plugins/incline.lua
@@ -0,0 +1,25 @@
+return {
+  'b0o/incline.nvim',
+  event = 'VeryLazy',
+  config = function()
+    -- Create custom highlight groups for transparent background
+    vim.api.nvim_set_hl(0, 'InclineNormalTransparent', {
+      fg = vim.api.nvim_get_hl(0, { name = "Comment" }).fg,
+      bg = 'NONE',
+    })
+
+    require('incline').setup({
+      highlight = {
+        groups = {
+          InclineNormal = 'InclineNormalTransparent',
+          InclineNormalNC = 'InclineNormalTransparent',
+        },
+      },
+      window = {
+        padding = 1,
+        margin = { vertical = 0, horizontal = 1 },
+      },
+    })
+  end,
+}
+
