c19e86d8522b5e00f27f06f7a05b86e706687ea4
Victor Aremu <me@victorare.mu>
2025-09-02 22:33:42 +0100

    refactor: themify and xeno

--- lua/plugins/themify.lua
+++ lua/plugins/themify.lua
@@ -1,7 +1,10 @@
 return {
 	"LmanTW/themify.nvim",
+	dependencies = {
+    "kyza0d/xeno.nvim"
+	},
 	lazy = false,
-	priority = 1000,
+	priority = 999,
 	keys = {
 		{
 			"<leader>b",
@@ -10,6 +13,23 @@ return {
 		},
 	},
 	config = function()
+		local xeno = require("xeno")
+		xeno.new_theme("xeno-lilypad", {
+			base = "#1E1E1E",
+			accent = "#8CBE8C",
+			contrast = 0.1,
+		})
+		xeno.new_theme("xeno-pink-haze", {
+			base = "#0f0c0e",
+			accent = "#D19EBC",
+			contrast = 0.1,
+		})
+		xeno.new_theme("xeno-golden-hour", {
+			base = "#11100f",
+			accent = "#FFCC33",
+			contrast = 0.1,
+		})
+
 		require("themify").setup({
 			"nyoom-engineering/oxocarbon.nvim",
 			"monochrome",
