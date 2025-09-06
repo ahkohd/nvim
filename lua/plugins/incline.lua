80c1a6a2cfee719b31c13638e30ad59d532edbab
Victor Aremu <me@victorare.mu>
2025-09-07 00:05:27 +0100

    update appearance

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -1,9 +1,9 @@
 return {
 	"b0o/incline.nvim",
-  dependencies= {
-    "nvim-treesitter/nvim-treesitter",
-    "SmiteshP/nvim-navic"
-  },
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"SmiteshP/nvim-navic",
+	},
 	event = "VeryLazy",
 	config = function()
 		-- Create custom highlight groups for transparent background
@@ -28,30 +28,34 @@ return {
 			window = {
 				padding = 1,
 				margin = { vertical = 0, horizontal = 1 },
+				placement = {
+					horizontal = "left",
+					vertical = "top",
+				},
 			},
 			render = function(props)
 				if props.focused then
-					local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
-					if filename == '' then
-						filename = '[No Name]'
+					local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+					if filename == "" then
+						filename = "[No Name]"
 					end
 					local ft_icon, ft_color = devicons.get_icon_color(filename)
 					local modified = vim.bo[props.buf].modified
 					local res = {
 						-- ft_icon and { ' ', ft_icon, ' ', guibg = 'NONE', guifg = ft_color } or '',
-						{ filename, gui = modified and 'italic' or '' },
-						guibg = 'NONE',
+						{ filename, gui = modified and "italic" or "" },
+						guibg = "NONE",
 					}
 					if props.focused then
 						for _, item in ipairs(navic.get_data(props.buf) or {}) do
 							table.insert(res, {
-								{ ' ➜ ', group = 'NavicSeparator' },
-								{ item.icon, group = 'NavicIcons' .. item.type },
-								{ item.name, group = 'NavicText' },
+								{ " ➜ ", group = "NavicSeparator" },
+								{ item.icon, group = "NavicIcons" .. item.type },
+								{ item.name, group = "NavicText" },
 							})
 						end
 					end
-					table.insert(res, ' ')
+					table.insert(res, " ")
 					return res
 				end
 
